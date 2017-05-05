a17ebea23ba914be59303a4c21e61b02a22b639f
Luke Dowell <Luke Dowell>
2017-05-05 15:05:09 -0500

    Starting problem 11

--- src/org/badgrades/euler/11LargestProductInAGrid.kt
+++ src/org/badgrades/euler/11LargestProductInAGrid.kt
@@ -1,8 +1,27 @@
 package org.badgrades.euler
 
+import java.io.File
+import java.util.*
+
 object `11LargestProductInAGrid` {
     
-    fun main(args: Array<String>) {
+    val gridHeight = 20
+    val gridWidth = 20
+    val stringInput = Scanner(File("res/problem11input.txt"))
     
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val grid = Array(gridHeight, { IntArray(gridWidth) })
+        
+        grid.gridIndexed { x, y, _ -> grid[x][y] = stringInput.nextInt() }
+        grid.gridIndexed { x, y, value -> println("X: $x Y: $y Val: $value") }
+    }
+    
+    fun Array<IntArray>.gridIndexed(operation: (x: Int, y: Int, value: Int) -> Unit) {
+        this.forEachIndexed { y, columns ->
+            columns.forEachIndexed { x, value ->
+                operation(x, y, value)
+            }
+        }
     }
 }
