629977caec026e6376197499a5dfd7fa584718e1
Luke Dowell <ldowell@grubhub.com>
2017-07-28 16:07:57 -0500

    Problem 12 finished

--- src/org/badgrades/euler/12HighlyDivisibleTriangularNumber.kt
+++ src/org/badgrades/euler/12HighlyDivisibleTriangularNumber.kt
@@ -4,12 +4,26 @@ object `12HighlyDivisibleTriangularNumber` {
     
     @JvmStatic
     fun main(args: Array<String>) {
-        val triangle = generateSequence(seed = Pair(1L, 1L), nextFunction = { (_, index) -> Pair(getTriangleNumber(index), index + 1L)})
+        val triangle = generateSequence(
+                seed = Pair(1L, 1L),
+                nextFunction = { Pair(getTriangleNumber(it.second + 1L), it.second + 1L) }
+        )
         val list = triangle.take(100_000).toList()
-        
-        list.first { getFactorsForNumber(it.first).size > 500 }
+
+        println(list.first { getFactorsForNumber(it.first).size > 500 }.first)
     }
     
     fun getTriangleNumber(index: Long) : Long = (0..index).sum()
-    fun getFactorsForNumber(number: Long) : List<Long> = (number..1).filter { number % it == 0L }.toList()
+
+    fun getFactorsForNumber(number: Long) : List<Long> {
+        val factors = mutableListOf<Long>()
+        val numberSqrt = Math.ceil(Math.sqrt(number.toDouble())).toLong()
+        (1L..numberSqrt).forEach {
+            if (number % it == 0L) {
+                factors.add(it)
+                factors.add(number / it)
+            }
+        }
+        return factors.distinct()
+    }
 }
