97079bf97f997fe4fe93d64533e025738ab6b530
Luke Dowell <ldowell@grubhub.com>
2017-08-09 16:35:00 -0500

    Problem 15 complete

--- src/org/badgrades/euler/15LatticePaths.kt
+++ src/org/badgrades/euler/15LatticePaths.kt
@@ -12,8 +12,27 @@ object `15LatticePaths` {
 
     @JvmStatic
     fun main(args: Array<String>) {
-
-
+        println("The answer to problem 15 is ${binomialCoefficient(40L, 20L)}")
         println("This completed in ${ChronoUnit.MILLIS.between(startingTime, LocalDateTime.now())} milliseconds")
+    }
+
+    /** https://rosettacode.org/wiki/Evaluate_binomial_coefficients#Java */
+    fun binomialCoefficient(n: Long, k: Long) : Long {
+        var n2 = n
+        var k2 = k
+
+        if (k > n - k)
+            k2 = n - k
+
+        var b = 1L
+        var i = 1
+        var m = n
+        while (i <= k2) {
+            b = b * m / i
+            i++
+            m--
+        }
+
+        return b
     }
 }
